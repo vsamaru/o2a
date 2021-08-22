9f01d87e410e46d6a08891cf0d8466bf98a4ff1e
broneukr <broneukr@gmail.com>
2021-08-22 17:22:59 +0300

    first commit

---
+++ public/script.js
@@ -0,0 +1,10 @@
+// const da = {
+//   list: [1, 2], 
+//   lastUpdate: Date.now()
+// } 
+
+// setInterval(() => {
+//   da.list.push(42);
+//   const event = new Event('custom');
+//   document.dispatchEvent(event)
+// }, 2000);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: "./index.js",
+    mode: "production",
+    node: {
+        fs: 'empty',
+        net: 'empty',
+        child_process: 'empty',
+        tls: 'empty',
+    },
+    optimization: {
+        minimize: false
+    },
+    output: {
+        filename: "worker.js"
+    },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [],
+  }
+}
