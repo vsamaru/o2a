61b16989bfc980cc3875b3f027f9f413ca986abe
broneukr <broneukr@gmail.com>
2021-08-23 15:58:18 +0300

    first commit

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ async function handleEvent(event) {
 
   var vv = await db.list()
  
-    var v = await vv.map(e => `<img src="${e.url}" onclick=fetch("/x/${e.id}")>`)
+    var v = await vv.map(e => `<img src="${e.url}" onclick=fetch("/x?i=${e.id}")>`)
     var w = await vv.map(e => `<figcaption>${e.pic}</figcaption><br>`)
     v = [...v,...w]
    // console.warn(v)
