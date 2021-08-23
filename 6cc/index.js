c6ef34246757793b859e0c8ebb536aaad95ae443
broneukr <broneukr@gmail.com>
2021-08-23 17:33:01 +0300

    first commit

--- 6cc/index.js
+++ 6cc/index.js
@@ -174,7 +174,7 @@ B.method = "sendMessage"
         if (re.location && !re.id && !re.result_id) {
             X.location = re.location.latitude + "," + re.location.longitude
             if (!X.ref) B.text = "NO REF"
-            X.geo = await fetch(`https://api.cloudinary.com/v1_1/o6/image/upload?tags=g&upload_preset=o6oooo&file=${encodeURIComponent(`https://www.mapquestapi.com/staticmap/v5/map?locations=${X.location}|marker-lg-black-${X.cc}-${X.ab}&zoom=19&size=640,320@2x&key=brX4s7eKqZr24Z1icIAJzRYOBQEWxtVv&type=sat`)}`).then(r => r.json()).then(r => {
+            X.geo = await fetch(`https://api.cloudinary.com/v1_1/o6/image/upload?tags=g&upload_preset=o6oooo&file=${encodeURIComponent(`https://www.mapquestapi.com/staticmap/v5/map?locations=${X.location}|marker-lg-black-${X.cc}-${X.ab}&zoom=19&size=640,240@2x&key=brX4s7eKqZr24Z1icIAJzRYOBQEWxtVv&type=sat`)}`).then(r => r.json()).then(r => {
                 return r.public_id
             })
             B.photo = "https://res.cloudinary.com/o6/" + X.geo
