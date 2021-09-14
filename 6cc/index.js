f6bc31bea276e7fece42f4fd04285fad868f4e65
vsamaru <vsamaru@gmail.com>
2021-09-15 00:28:03 +0300

    first commit

--- 6cc/index.js
+++ 6cc/index.js
@@ -35,7 +35,7 @@
             chat_id: re.chat
         }
         // if (re.chat != 465081843) {
-        //     console.error(re)
+        //     console.error(re)c
         //     return new Response({
         //         status: 200
         //     })
